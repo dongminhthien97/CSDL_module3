d1efe96de6829416a72bcdd04d21dbc331571c37
Minhthien <dongminhthiendn97@gmail.com>
2025-07-25 21:28:02 +0700

    case_study_5

--- Case_Study/CaseStudy.sql
+++ Case_Study/CaseStudy.sql
@@ -274,6 +274,37 @@ GROUP BY
 ORDER BY 
     so_lan_dat_phong ASC;
     
+-- 5. Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+SELECT 
+    kh.ma_khach_hang,
+    kh.ho_ten,
+    lk.ten_loai_khach,
+    hd.ma_hop_dong,
+    dv.ten_dich_vu,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    dv.chi_phi_thue + IFNULL(SUM(hdct.so_luong * dvdk.gia), 0) AS tong_tien
+FROM 
+    khach_hang kh
+LEFT JOIN 
+    loai_khach lk ON kh.ma_loai_khach = lk.ma_loai_khach
+LEFT JOIN 
+    hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+LEFT JOIN 
+    dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+LEFT JOIN 
+    hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+LEFT JOIN 
+    dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+GROUP BY 
+    kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, 
+    hd.ma_hop_dong, dv.ten_dich_vu, 
+    hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, dv.chi_phi_thue
+ORDER BY 
+    kh.ma_khach_hang, hd.ma_hop_dong;
+
+
+
     
     
 
